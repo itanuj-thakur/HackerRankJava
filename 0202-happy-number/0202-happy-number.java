57a27b1e34bc73f6a210ab41b5223fe6e5fc1e3b
Tanuj <tanujthakur634@gmail.com>
2026-09-11 12:53:33 +0530

    Time: 1 ms (77.51%), Space: 42.4 MB (50.59%) - LeetHub - Time: O(log n), Space: O(1)

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,24 +1,15 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public boolean isHappy(int n) {
-        Set<Integer> seen = new HashSet<>();
-
-        while (true) {
-            int s = 0;
-            while (n != 0) {
+        while (n != 1 && n != 4) {
+            int sum = 0;
+            while (n > 0) {
                 int d = n % 10;
-                s += d * d;
+                sum += d * d;
                 n /= 10;
             }
-
-            if (s == 1) return true;
-            if (seen.contains(s)) return false;
-
-            seen.add(s);
-            n = s;
+            n = sum;
         }
+        return n == 1;
     }
 }
 
